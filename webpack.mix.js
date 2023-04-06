ef80dfdf453cb66463f966019d3c2da42a98e39e
Seraphic <danilsocol-2002@mail.ru>
2023-04-06 15:00:09 +0500

    fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/messenger.js', 'public/js')
     .js('resources/js/bootstrap.js', 'public/js')
+    .js('resources/js/admin.js','public/js')
     .postCss('resources/css/app.css', 'public/css', [])
     .postCss('resources/css/auth.css', 'public/css', [])
     .postCss('resources/css/reg.css', 'public/css', [])
@@ -10,4 +11,5 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/reset.css', 'public/css', [])
     .postCss('resources/css/authentication.css', 'public/css', [])
     .postCss('resources/css/forgot-password.css', 'public/css', [])
+    .postCss('resources/css/admin.css', 'public/css', [])
     .postCss('resources/css/welcome.css', 'public/css', []);
